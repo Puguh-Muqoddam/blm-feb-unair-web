c9a9ad52c9cb0f65f9525c3822c99f38c34fb791
Puguh Muqoddam <puguhmuqoddam151@gmail.com>
2026-08-28 09:48:02 +0700

    Ubah alur Kotak Aspirasi ke Bank Aspirasi (GForm) dan hapus Peminjaman Ruangan

--- rename_html_links.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path . -Filter *.html
-
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName -Raw
-    
-    $content = $content -replace "selayangpandang\.html", "tentang-blm.html"
-    
-    Set-Content -Path $file.FullName -Value $content -Encoding UTF8
-}
